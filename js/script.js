7f62d4387a0b709aa20f165b280e71ce59097902
yuridido <yuridido@yahoo.it>
2020-07-06 15:49:37 +0200

    esercizio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var nome = prompt("qual\'è il tuo nome?");
+var cognome = prompt("qual\'è il tuo cognome?");
+var colorePreferito = prompt("qual\'è il tuo colore preferito?");
+var numero = 19;
+var password = nome + cognome + colorePreferito + numero;
+
+// alert(password);
+
+console.log(nome);
+console.log(cognome);
+console.log(colorePreferito);
+console.log(numero);
+console.log(password);
+
+document.getElementById('pass').innerHTML= password;
+
+// document.writeln(password);
