892ecda2501cda441bc1c9b90603c7126ffad80b
yuridido <yuridido@yahoo.it>
2020-07-07 10:32:31 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var nome = prompt("qual\'è il tuo nome?");
-var cognome = prompt("qual\'è il tuo cognome?");
-var colorePreferito = prompt("qual\'è il tuo colore preferito?");
+var nome = prompt("qual è il tuo nome?");
+var cognome = prompt("qual è il tuo cognome?");
+var colorePreferito = prompt("qual è il tuo colore preferito?");
 var numero = 19;
 var password = nome + cognome + colorePreferito + numero;
 
